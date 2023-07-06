54406b7a83aaf9779f6ab362dba8c4e0e51aec76
Chris <filmee24@gmail.com>
2023-07-06 18:15:50 +0200

    feat: Add RemovePageProccessor

---
+++ Proccessing/Proccessors/RemovePageProccessor.cs
@@ -0,0 +1,13 @@
+﻿using PdfSharpCore.Pdf;
+
+namespace PDF_TOC.Proccessing;
+
+public class RemovePageProccessor : IPdfProccessor
+{
+    public int Page { get; set; }
+    
+    public void Invoke(PdfDocument document, PdfProccessor processor)
+    {
+        document.Pages.RemoveAt(Page);
+    }
+}
