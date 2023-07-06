07d1d011ded9d93bd793b71664f116f861ef8b64
Chris <filmee24@gmail.com>
2023-07-06 19:10:29 +0200

    feat: Add PDF Protection Proccessor

---
+++ Proccessing/Proccessors/ProtectionProccessor.cs
@@ -0,0 +1,20 @@
+﻿using PdfSharpCore.Pdf;
+
+namespace PDF_TOC.Proccessing.Proccessors;
+
+public class ProtectionProccessor : IPdfProccessor
+{
+    private readonly string userPw, ownerPw;
+
+    public ProtectionProccessor(string userPw, string ownerPw)
+    {
+        this.userPw = userPw;
+        this.ownerPw = ownerPw;
+    }
+
+    public void Invoke(PdfDocument document, PdfProccessor processor)
+    {
+        document.SecuritySettings.UserPassword = userPw;
+        document.SecuritySettings.OwnerPassword = ownerPw;
+    }
+}
